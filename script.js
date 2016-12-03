f88a8bf2040ce1b71a5ef9cd87ff7b7b477c3cbe
sammyandharry <14wangh@kgv.hk>
2016-12-02 22:17:15 -0500

    done!

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	window.sr=ScrollReveal();
+	//sr.reveal('.container', {delay:200});
+	sr.reveal('#journey', {delay:2000});
+	sr.reveal('#dance', {delay:150});
+	sr.reveal('#dancet', {delay:250});
+	sr.reveal('#lift', {delay:150});
+	sr.reveal('#lift1', {delay:250});
+	sr.reveal('#bae', {delay:150});
+	sr.reveal('#fun', {delay:150});
+	sr.reveal('#funbae', {delay:150});
+});
